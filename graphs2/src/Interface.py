0e8a5ab42461a771c8c91f113e1b27d0927303ae
Joberth Rogers <joberth.rogers18@gmail.com>
2019-04-16 12:35:18 -0400

    Added connection with database Adjusting to get the input people

--- graphs2/src/Interface.py
+++ graphs2/src/Interface.py
@@ -2,6 +2,7 @@ import Tkinter as tk
 import pydotplus as ptp
 from IPython.display import SVG, display
 from PIL import ImageTk, Image
+from connectionDatabse import Connection
 
 class Application:
 
@@ -9,56 +10,72 @@ class Application:
 
         self.containerEntry = tk.Frame(master)
         self.containerEntry.pack()
-        generateInterfaceEntry(self.containerEntry)
-        generateGraph()
+
+        Connection.connectionDatabase()
+
+        self.labelCodeCourse = tk.Label(self.containerEntry,
+                                   text = "Codigo da habilitacao: ")
+        self.labelCodeCourse.pack(side=tk.LEFT)
+        self.codeCourse = tk.Entry(self.containerEntry)
+        self.codeCourse["width"] = 30
+        self.codeCourse.pack(side=tk.LEFT, padx=10)
+
+        self.buttonCodeCourse = tk.Button(self.containerEntry)
+        self.buttonCodeCourse["text"] = "Procurar"
+        self.buttonCodeCourse["command"] = self.generateGraph
+        self.buttonCodeCourse.pack(side=tk.LEFT)
 
         self.containerGraph = tk.Frame(master,bg = "white")
         self.containerGraph.pack(pady=20)
-        showGraph(self.containerGraph, master)
+        self.showGraph(self.containerGraph, master)
 
-def generateInterfaceEntry(container):
+    def showGraph(self,container, master):
+        canvas = tk.Canvas(container,
+                                width= master.winfo_screenwidth(),
+                                height= master.winfo_screenheight() - 200,
+                                bg = "white")
+        img = tk.PhotoImage(file="file.png")
+        canvas.create_image(20, 
+                            (master.winfo_screenheight() - 200) // 8,
+                            anchor=tk.NW, 
+                            image=img)
+        canvas.image = img    
+        canvas.pack()
 
-    labelCodeCourse = tk.Label(container, text = "Codigo materia: ")
-    labelCodeCourse.pack(side=tk.LEFT)
-    codeCourse = tk.Entry(container)
-    codeCourse["width"] = 30
-    codeCourse.pack(side=tk.LEFT, padx=10)
+    def generateGraph(self):
+        
+        habilitationCode = self.codeCourse.get()
+        print(habilitationCode)
 
-    buttonCodeCourse = tk.Button(container)
-    buttonCodeCourse["text"] = "Procurar"
-    buttonCodeCourse.pack(side=tk.LEFT)
+        database = Connection.connectionDatabase()
+        collectionCourse = database['habilitations']
 
-def showGraph(container, master):
-    canvas = tk.Canvas(container,
-                            width= master.winfo_screenwidth(),
-                            height= master.winfo_screenheight() - 200,
-                            bg = "white")
-    img = tk.PhotoImage(file="file.png")
-    canvas.create_image(20, 
-                        (master.winfo_screenheight() - 200) // 8,
-                        anchor=tk.NW, 
-                        image=img)
-    canvas.image = img    
-    canvas.pack()
+        document = collectionCourse.find()
+        print(document.count())
 
-def generateGraph():
 
-    graph = ptp.Dot(graph_type='graph', rankdir='LR')
-    edges = [(1,2), (1,3), (2,4), (2,5), (3,5)]
-    nodes = [(1, "A", ""), (2, "B", ""), (3, "C", ""), (4, "D", ""), (5, "E", "")]
-    for e in edges:
-        graph.add_edge(ptp.Edge(e[0], e[1]))
-    for n in nodes:
-        node = ptp.Node(name=n[0], label= n[1], fillcolor=n[2], style="filled" )
-        graph.add_node(node)
-    graph.write_png('file.png')
     
-root = tk.Tk() # Allow the widget be on the table
+        '''
+        graph = ptp.Dot(graph_type='graph', rankdir='LR')
+        edges = [(1,2), (1,3), (2,4), (2,5), (3,5)]
+        nodes = [(1, "A", ""), (2, "B", ""), (3, "C", ""), (4, "D", ""), (5, "E", "")]
+        for e in edges:
+            graph.add_edge(ptp.Edge(e[0], e[1]))
+        for n in nodes:
+            node = ptp.Node(name=n[0], label= n[1], fillcolor=n[2], style="filled" )
+            graph.add_node(node)
+        graph.write_png('file.png')
+        '''
 
-root.title('Trabalho 2 Grafo')
-root.geometry('700x700')
+
+if __name__ == "__main__":
+  
+    root = tk.Tk() # Allow the widget be on the table
+
+    root.title('Trabalho 2 Grafo')
+    root.geometry('700x700')
 
 
-Application(root) # Pass the root with config to Application class
-root.mainloop() # loop for aba
+    Application(root) # Pass the root with config to Application class
+    root.mainloop() # loop for aba
 
