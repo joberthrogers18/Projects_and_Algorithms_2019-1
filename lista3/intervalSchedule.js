29bd3c25bd12c63ae5b76caa10670d4a17e4d04b
Joberth Rogers <joberth.rogers18@gmail.com>
2019-04-25 23:26:47 -0400

    Finished interval schedule

--- lista3/intervalSchedule.js
+++ lista3/intervalSchedule.js
@@ -1,30 +1,34 @@
 
-const defineGreaterHour = (date1, date2) => {
+const defineGreaterHour = (date1, date2, labelDate1, labelDate2) => {
     date1Format = new Date()
     date2Format = new Date()
-    splitDate1 = date1.final.split(':')
-    splitDate2 = date2.final.split(':')
-    console.log('passou')
+    splitDate1 = date1[labelDate1].split(':')
+    splitDate2 = date2[labelDate2].split(':')
     date1Format.setHours(splitDate1[0], splitDate1[1])
     date2Format.setHours(splitDate2[0], splitDate2[1])
-    console.log(date1Format)
-    console.log(date2Format)
-    return ((date1Format > date2Format) ? 1 : -1)
+    return ((date1Format >= date2Format) ? 1 : 0)
 }
 
 const intervalSchedule = () => {
-    var times = [{"start": '05:00', "final": '15:03'}, {"start": '10:30', "final": '14:45'}, {"start": '02:30', "final": '06:40'}]
+    var times = [{"start": '01:25', "final": '06:40'}, {"start": '05:45', "final": '20:45'}, {"start": '07:30', "final": '10:40'}]
 
     var schedule = []
 
-    if(schedule == []){
-        schedule.push(times[0])
-        times.pop(0)
-    }
+    times.sort((elm1, elm2) => {
+        defineGreaterHour(elm1, elm2, 'final', 'final')
+    })
 
-    times.sort(defineGreaterHour)
 
-    console.log(times)
+    schedule.push(times[0])
+    times.shift()
+    
+    times.map(item => {
+       if(defineGreaterHour(item, schedule[schedule.length - 1], 'start', 'final')){
+            schedule.push(item)   
+       }
+    })
+
+    console.log(schedule)
 
 }
 
