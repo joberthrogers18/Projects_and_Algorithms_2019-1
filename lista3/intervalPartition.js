4fc9a17b4046e597d62b5096d5af6e83b4460ca5
Joberth Rogers <joberth.rogers18@gmail.com>
2019-05-01 21:29:25 -0400

    Got to do interval partition

--- lista3/intervalPartition.js
+++ lista3/intervalPartition.js
@@ -2,7 +2,8 @@
 
 const BinaryHeap = require('@tyriar/binary-heap');
 
-all_times = [{ "start": '01:25', "final": '06:40' }, { "start": '07:45', "final": '10:45' }, { "start": '11:30', "final": '20:40' }]
+all_times = [{ "start": '01:25', "final": '06:40' }, { "start": '05:45', "final": '10:45' }, { "start": '11:30', "final": '20:40' },
+{ "start": '02:00', "final": '07:00' }, { "start": '20:10', "final": '21:00' }, { "start": '15:00', "final": '23:10' }]
 
 //last_time = new Heap();
 
@@ -37,12 +38,21 @@ const intervalPartition = (time) => {
             heapEarlyEnd.insert(node.key, 0);
             classes.push(aux);
         } else {
-            console.log('----------------------------')
-            if(heapEarlyEnd.findMinimum().key <= heap.findMinimum().key){
+            console.log('--------------');
+            console.log(heapEarlyEnd.findMinimum().key)
+            console.log(heap.findMinimum().key)
+            if(heapEarlyEnd.findMinimum().key <= time[heap.findMinimum().value].start){
                 let node = heap.extractMinimum();
                 let currentEarly = heapEarlyEnd.extractMinimum()
                 classes[currentEarly.value].push(time[node.value]);
                 heapEarlyEnd.insert(time[node.value].final , currentEarly.value);
+            }
+            else{
+                let node = heap.extractMinimum();
+                aux = [];
+                aux.push(time[node.value]);
+                classes.push(aux);
+                heapEarlyEnd.insert(node.key,classes.length - 1);
             }
             
             //let nodeAux = heap.extractMinimum();
