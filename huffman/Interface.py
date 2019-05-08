a0ae950fa2ce9ceb42ab087c655e1356a2f50ee6
Joberth Rogers <joberth.rogers18@gmail.com>
2019-05-08 10:36:21 -0400

    Finished the layout screen in huffman program

--- huffman/Interface.py
+++ huffman/Interface.py
@@ -3,6 +3,10 @@ import tkinter as tk
 
 class Application:
     def __init__(self, master=None):
+
+        self.width_screen = master.winfo_width()
+        self.height_screen = master.winfo_height()
+
         self.container1 = tk.Frame(master)
         self.container1.pack()
         self.label_title = tk.Label(self.container1, text="Winrar FLEX")
@@ -15,12 +19,14 @@ class Application:
             self.container2, text="Compactar/Descompactar")
         self.encode_button.pack(pady="30")
 
-        self.container3 = tk.Frame(master, background="white", height=200, width= master.winfo_screenwidth() - 200)
-        self.container3.pack()
+        self.container3 = tk.Frame(
+            master, background="white", height=350, width=self.width_screen - 20)
+        self.container3.pack(pady=20)
 
 
 root = tk.Tk()
 root.title("Winrar FLEX")
-root.geometry("600x300+500+300")
+root.geometry("800x400+500+300")
+root.update()
 Application(root)
 root.mainloop()
