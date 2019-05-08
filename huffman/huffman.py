c8f9b105bcb420706527c7ad366a16794ea50a7f
Joberth Rogers <joberth.rogers18@gmail.com>
2019-05-07 21:18:10 -0400

    Added option to encode and dencoded a file by huffman

--- huffman/huffman.py
+++ huffman/huffman.py
@@ -1,26 +1,28 @@
-from heapq import heappush, heappop, heapify
-from collections import defaultdict
-import operator
+import os
 
 
 class Huffman:
 
-    def __init__(self, word):
+    def __init__(self, word, trim=(), dencode=1):
+        print(type(trim))
         self.word = word
         self.frequence = {}
         self.sort_frequence = []
         self.codes = {}
-        self.trim = ()
+        self.trim = trim
         self.word_encode = ''
         self.word_decode = ''
 
-        self.define_frequence()
-        self.sortFrequence()
-        self.buildTree()
-        self.trim = self.trimTree(self.sort_frequence)
-        self.assignCodes(self.trim)
-        self.encode()
-        self.decode()
+        if dencode:
+            self.define_frequence()
+            self.sortFrequence()
+            self.buildTree()
+            self.trim = self.trimTree(self.sort_frequence)
+            self.assignCodes(self.trim)
+            self.encode()
+        else:
+            self.word_encode = word
+            self.decode()
 
     def define_frequence(self):
         # Function to take the number of character in phrase tha will
@@ -42,29 +44,31 @@ class Huffman:
 
         for key in keys_frequence:
             self.sort_frequence.append((self.frequence[key], key))
-        
+
         self.sort_frequence.sort()
 
     def buildTree(self):
 
         while len(self.sort_frequence) > 1:
-            the_firsts = self.sort_frequence[0 : 2]
-            rest = self.sort_frequence[2: ]
+            the_firsts = self.sort_frequence[0: 2]
+            rest = self.sort_frequence[2:]
             combine_freq = the_firsts[0][0] + the_firsts[1][0]
             rest.append((combine_freq, the_firsts))
-            rest.sort(key = lambda tup: tup[0])
+            rest.sort(key=lambda tup: tup[0])
             self.sort_frequence = rest
-        
+
         self.sort_frequence = self.sort_frequence[0]
 
     def trimTree(self, tree):
 
         p = tree[1]
-        if type(p) == type("") : return p
-        else: return (self.trimTree(p[0]),self.trimTree(p[1]))
+        if type(p) == type(""):
+            return p
+        else:
+            return (self.trimTree(p[0]), self.trimTree(p[1]))
 
-    def assignCodes(self,node, pat=''):
-        
+    def assignCodes(self, node, pat=''):
+
         if type(node) == type(""):
             self.codes[node] = pat
         else:
@@ -73,23 +77,56 @@ class Huffman:
 
     def encode(self):
         for char in self.word:
-            self.word_encode += self.codes[char] 
-    
+            self.word_encode += self.codes[char]
+
     def decode(self):
-        
+
         p = self.trim
 
         for bit in self.word_encode:
-            if bit == '0': p = p[0]
-            else : p = p[1]
+            if bit == '0':
+                p = p[0]
+            else:
+                p = p[1]
             if type(p) == type(""):
                 self.word_decode += p
                 p = self.trim
-        
 
-        
+
 if __name__ == "__main__":
 
-    huffman = Huffman('Melanie eu te amo')
-    print(huffman.codes)
-    print(huffman.word_decode)
+    file = open("teste_encoded.txt", "r")
+
+    first_line = file.readline()
+
+    if first_line[:2] == "*(":
+
+        word_dencode = ''
+
+        for line in file:
+            word_dencode += line
+
+        dencode = Huffman(word=word_dencode, trim=eval(
+            first_line[1:]), dencode=0)
+
+        file_dencode = open(os.path.basename(file.name)[
+                            0: -4]+"_dencoded.txt", "w+")
+        file_dencode.write(dencode.word_decode)
+
+    else:
+        file.seek(0)
+
+        word_for_encode = ''
+
+        for line in file:
+            word_for_encode += line
+
+        encodefy = Huffman(word=word_for_encode)
+
+        file_encode = open(os.path.basename(file.name)[
+                           0: -4] + "_encoded.txt", "w+")
+
+        file_encode.write("*" + str(encodefy.trim) + "\n")
+        file_encode.write(str(encodefy.word_encode))
+
+    file.close()
